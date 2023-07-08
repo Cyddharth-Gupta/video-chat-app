2984375cca19679b75dd2f79624bd946a78d6448
Cyddharth-Gupta <cyddharthgupta@gmail.com>
2023-07-08 20:15:28 +0530

    Delete client dir

--- server/index.js
+++ server/index.js
@@ -2,3 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { Server } from 'socket.io';
 
+const io = new Server(8001);
+const app = express();
+
+io.on('connection', (socket) => {
+    console.log("Socket connected at ", socket.id)
+})
+
+app.listen(8000, () => {
+    console.log('Server is running at 8000');
+})
+io.listen(8001)
